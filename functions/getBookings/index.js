91c297f30df516f3af0b6fe538855d0bd339528c
Winnie <146666846+W-nn-e@users.noreply.github.com>
2024-09-12 10:58:15 +0200

    added comment

--- functions/getBookings/index.js
+++ functions/getBookings/index.js
@@ -2,21 +2,25 @@
 const { db } = require("../../services/db.js");
 const { sendError, sendResponse } = require("../../responses/index.js");
 
+// Function to fetch bookings from the database
 async function getBooking() {
+  // Performs a scan operation on the Bonzai-booking table and retrieves all items
     const {Items} = await db.scan({
         TableName: 'Bonzai-booking' 
     })
+    // Returns the fetched items
     return Items
 }
 
 exports.handler = async (event) => {
-  
+    // Retrieve bookings by calling on the getBooking function
     const bookings = await getBooking()
 
   try {
- 
+    // If the data is retrieved successfully, return a success response with the data
     return sendResponse({message: 'success', bookings})
   } catch (error) {
+    // Or if an error occurs, return an error response
     console.error("Error retrieving bookings: ", error);
     return sendError(500, {
       message: "An error occurred while retrieving the bookings",
